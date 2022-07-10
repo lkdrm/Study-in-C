8e12d6c5f121e6e89bf2dfe84ffbd2a836307960
LkDrm(Vitalik) <larik115@gmail.com>
2022-07-10 19:06:25 +0200

    Finish tasks

--- BaseofApiString/BaseofApiString/Program.cs
+++ BaseofApiString/BaseofApiString/Program.cs
@@ -122,59 +122,210 @@ static void StrBuilder()
     Console.WriteLine(str);
 }
 
-string name = "John";
-int age = 26;
+static void FormatStr()
+{
 
-string str1 = string.Format("My name is {0} & I`m {1}.", name, age);
-//or
-string str2 = "My name is " + name + " and I`m " + age;
-//or
-string str3 = $"My name is {name} & I`m {age}.";
+    string name = "John";
+    int age = 26;
 
-Console.WriteLine(str1);
-Console.WriteLine(str3);
+    string str1 = string.Format("My name is {0} & I`m {1}.", name, age);
+    //or
+    string str2 = "My name is " + name + " and I`m " + age;
+    //or
+    string str3 = $"My name is {name} & I`m {age}.";
 
-string str4 = "My name is \nJohn.";
-Console.WriteLine(str4);
+    Console.WriteLine(str1);
+    Console.WriteLine(str3);
 
-string str5 = "I`m \t 26.";
-Console.WriteLine(str5);
+    string str4 = "My name is \nJohn.";
+    Console.WriteLine(str4);
 
-string str6 = $"My name is {Environment.NewLine}John.";
-Console.WriteLine(str6);
+    string str5 = "I`m \t 26.";
+    Console.WriteLine(str5);
 
-string str7 = "I`m a John & i`m a 'good' programmer.";
-Console.WriteLine(str7);
+    string str6 = $"My name is {Environment.NewLine}John.";
+    Console.WriteLine(str6);
 
-string str8 = "C:\\tmp\\test_file.txt";
-//or
-string str9 = @"C:\tmp\test_file.txt";
-Console.WriteLine(str8);
-Console.WriteLine(str9);
+    string str7 = "I`m a John & i`m a 'good' programmer.";
+    Console.WriteLine(str7);
+
+    string str8 = "C:\\tmp\\test_file.txt";
+    //or
+    string str9 = @"C:\tmp\test_file.txt";
+    Console.WriteLine(str8);
+    Console.WriteLine(str9);
 
 
-int answer = 42;
-string result = string.Format("{0:d}", answer);
-string result2 = string.Format("{0:d4}", answer);
-Console.WriteLine(result);
-Console.WriteLine(result2);
+    int answer = 42;
+    string result = string.Format("{0:d}", answer);
+    string result2 = string.Format("{0:d4}", answer);
+    Console.WriteLine(result);
+    Console.WriteLine(result2);
 
-result = string.Format("{0:f}", answer);
-result2 = string.Format("{0:f4}",answer);
-Console.WriteLine(result);
-Console.WriteLine(result2);
+    result = string.Format("{0:f}", answer);
+    result2 = string.Format("{0:f4}", answer);
+    Console.WriteLine(result);
+    Console.WriteLine(result2);
 
-Console.OutputEncoding = Encoding.UTF8;
+    Console.OutputEncoding = Encoding.UTF8;
 
-double money = 12.8;
+    double money = 12.8;
 
-result = string.Format("{0:C}", money);
-result2 = string.Format("{0:C2}", money);
-Console.WriteLine(result);
-Console.WriteLine(result2);
-//or
-Console.WriteLine(money.ToString("C2"));
+    result = string.Format("{0:C}", money);
+    result2 = string.Format("{0:C2}", money);
+    Console.WriteLine(result);
+    Console.WriteLine(result2);
+    //or
+    Console.WriteLine(money.ToString("C2"));
 
-Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+    Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
 
-Console.WriteLine(money.ToString("C2"));
+    Console.WriteLine(money.ToString("C2"));
+}
+
+static void equalStrings()
+{
+
+    string str1 = "abcde";
+    string str2 = "abcde";
+
+    bool areEqual = str1 == str2;
+    Console.WriteLine(areEqual);
+
+    areEqual = string.Equals(str1, str2, StringComparison.Ordinal);
+    Console.WriteLine(areEqual);
+
+    string s1 = "Strasse";
+    string s2 = "Straße";
+
+    areEqual = string.Equals(s1, s2, StringComparison.Ordinal);// most usefull
+    Console.WriteLine(s1);
+    Console.WriteLine(areEqual);
+
+    areEqual = string.Equals(s1, s2, StringComparison.InvariantCulture);
+    Console.WriteLine(areEqual);
+
+    areEqual = string.Equals(s1, s2, StringComparison.CurrentCulture);
+    Console.WriteLine(areEqual);
+}
+
+static void inputString()
+{
+    //Console.WriteLine("Hi, please tell me your name: ");
+
+    //string name = Console.ReadLine();
+    //string sentence = $"Your name is {name}";
+    //Console.WriteLine(sentence);
+
+    Console.WriteLine("Hi, please tell me your age: ");
+    string input = Console.ReadLine();
+    int age = int.Parse(input);
+
+    string sentence = $"Your age is {age}";
+    Console.WriteLine(sentence);
+
+    Console.Clear();
+    Console.BackgroundColor = ConsoleColor.Cyan;
+    Console.ForegroundColor = ConsoleColor.Magenta;
+
+    Console.Write("New style ");
+    Console.Write("New style\n");
+}
+
+static void parsing()
+{
+    byte b = 3; // 0000 0011
+    int i = b; // 0000 0000 0000 0000 0000 0000 0000 0011
+    long l = i; // 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0011
+    Console.WriteLine(b);
+
+    float f = i; // 3.0
+
+    b = (byte)i;
+    Console.WriteLine(b);
+
+    i = (int)f;
+    Console.WriteLine(i);
+
+    string str = "1";
+    // i = (int)str;
+
+    i = int.Parse(str);
+    Console.WriteLine($"Parsed i = {i}");
+
+    int x = 5;
+    int result = x / 2;
+    Console.WriteLine(result);
+
+    double result2 = (double)x / 2;
+    Console.WriteLine(result2);
+}
+
+static void comments()
+{
+    // a single-line comment
+
+    /*
+     * Multi-line comment
+     * We can write here many words
+     * 
+     */
+
+    // describe how is and why is.
+}
+
+static void moduleMath()
+{
+
+    Console.WriteLine(Math.Pow(2, 3));
+    Console.WriteLine(Math.Sqrt(9));
+    Console.WriteLine(Math.Sqrt(8));
+    Console.WriteLine(Math.Round(1.7));
+    Console.WriteLine(Math.Round(1.4));
+    Console.WriteLine($"Just round : {Math.Round(2.5)}");
+    Console.WriteLine($"Round with away from zero : {Math.Round(2.5, MidpointRounding.AwayFromZero)}");
+    Console.WriteLine($"Round to even {Math.Round(2.5, MidpointRounding.ToEven)}");
+}
+
+static void somethingAboutMassive()
+{
+    int[] a1; // created a masive type int
+    a1 = new int[10]; // only two elements
+
+    int[] a2 = new int[5];
+
+    int[] a3 = new int[5] { 1, 2, -3, 5, 10 };
+
+    int[] a4 = { 1, 3, 2, 4, 5 };
+
+    Console.WriteLine(a4[2]);
+
+    a4[4] = 6;
+    Console.WriteLine(a4[4]);
+    Console.WriteLine(a4.Length);
+    Console.WriteLine(a4[a4.Length - 1]); // find a last index in massive
+
+    string s1 = "abcdef";
+    char first = s1[0];
+    char last = s1[s1.Length - 1];
+    Console.WriteLine($"The first index of str is : {first}");
+    Console.WriteLine($"The last index of str is : {last}");
+    Console.WriteLine($"The length of str is : {s1.Length}");
+}
+
+static void aboutDatetime()
+{
+    DateTime now = DateTime.Now;
+    Console.WriteLine(now.ToString());
+
+    Console.WriteLine($"It`s {now.Date}");
+
+    DateTime dt = new DateTime(2016, 2, 28);
+    DateTime newDt = dt.AddDays(1);
+    Console.WriteLine(newDt);
+
+    TimeSpan ts = now - dt;
+    //ts = now.Subtract(dt);
+
+    Console.WriteLine(ts.TotalDays);
+}
---
+++ SomeTasks/SomeTasks/Program.cs
@@ -0,0 +1,109 @@
+﻿// 1 task :
+// Make a input & user write his/her name 
+// And the programm wil write hello {name}.
+static void firstTask()
+{
+    Console.WriteLine("Hello enter below please your name :");
+
+    string userName = Console.ReadLine();
+    Console.WriteLine($"Hello {userName}");
+}
+
+// 2 task :
+// Make user to write 2 digits & save like var1 & var2 console this variable 
+// than change str1 to str2
+// and console it.
+static void secondTask()
+{
+    Console.WriteLine("Write here your first integer :");
+    int a = int.Parse(Console.ReadLine());// 2
+
+    Console.WriteLine("Write here your second integer :");
+    int b = int.Parse(Console.ReadLine());// 3
+    Console.WriteLine($"Your first integer is a={a}, your second integer is b={b}");
+
+    int c = a;
+    a = b;
+    b = c;
+    Console.WriteLine($"Your first integer is a={a}, your second integer is b={b}");
+}
+
+// 3 task :
+// The user enter a integer like 123 , have 3. and a program input how much integers are in the var
+static void findLandOfnumb()
+{
+    Console.WriteLine("Enter below your integer :");
+    string a = Console.ReadLine();
+
+    Console.WriteLine($"This number have {a.Length} integer.");
+    // Or 
+
+    Console.WriteLine("Enter below your integer :");
+    int number = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"This number number have {number.ToString().Length}");
+}
+
+// 4 task :
+// Find S of a triangle, use a Geron`s formula.
+static void GeronsFormula()
+{
+    // S = sqrt (p*(p-a)*(p-b)*(p-c)
+    // p = (a+b+c)/2
+    Console.WriteLine("This program will find out a S of a trigle by using a Geron`s formula.");
+
+    Console.WriteLine("Enter please a first value: ");
+    float a = float.Parse(Console.ReadLine());
+    Console.WriteLine("Enter please a second value: ");
+    float b = float.Parse(Console.ReadLine());
+    Console.WriteLine("Enter please a third value: ");
+    float c = float.Parse(Console.ReadLine());
+
+    double p = (a + b + c) / 2;
+
+    double S = Math.Sqrt(p * ((p - a) * (p - b) * (p - c)));
+
+    Console.WriteLine($"The square is {Math.Round(S)}");
+}
+
+// 5 task:
+/* The user must to write :
+ *  Name , Last name, Age, Weight, Height.
+ *  Count his index of body : weight/(Height*Height)
+ *  The output must be :
+ *  Your profile :
+ *  Full name : Name , Last name.
+ *  Age : Age,
+ *  Weight : Weight,
+ *  Height : Height,
+ *  Body Mass Index : BMI.
+ */
+
+Console.WriteLine("We will create your profile");
+
+Console.WriteLine("Enter your First name:");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Enter your Last name:");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Enter your age:");
+int age = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your weight:");
+int weight = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your height in meters:");
+double height = double.Parse(Console.ReadLine());
+
+double bodyMassIndex = weight / (height * height);
+
+string profile =
+    $"Your profile:\n"
+   + $"Full name: {lastName} {firstName}\n"
+   + $"Age: {age}\n"
+   + $"Weight: {weight}\n"
+   + $"Height: {height}\n"
+   + $"Body Mass Index: {bodyMassIndex}";
+
+Console.WriteLine(profile);
