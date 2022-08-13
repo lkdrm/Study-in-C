cfc22e7f573e14e6b953534406e0030173760386
LkDrm(Vitalik) <larik115@gmail.com>
2022-08-13 21:19:07 +0200

    Read somethink about manage and unmanage files...

---
+++ OOP/OOP/ManageResource.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32.SafeHandles;
+
+namespace OOP
+{
+    public class ResourceHolder : IDisposable
+    {
+        private readonly SafeHandle managedResource;
+        
+        public ResourceHolder()
+        {
+            managedResource = new SafeFileHandle(new IntPtr(), true);
+        }
+
+        public void Dispose()
+        {
+            managedResource?.Dispose();
+            //GC.SuppressFinalize(this);
+        }
+    }
+}
+// enough to use IDisposible = with managaResource
+// without unmanage
